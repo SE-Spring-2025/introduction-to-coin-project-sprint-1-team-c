048de9108b01f0d5eedd4c2ed8035dc57dce9a53
Ayden Matocha <aydenmatocha@gmail.com>
2025-03-26 12:49:31 -0400

    Added code for constructors of Penny, Nickel, Dime

--- src/main/Nickel.java
+++ src/main/Nickel.java
@@ -1,9 +1,28 @@
 public class Nickel extends Coin {
     public Nickel() {
-
+        familiarName = "Nickel";
+	    frontImage = "T_Jefferson";
+	    backImage = "Jefferson_Memorial";
+	    valueDescription = "FIVE CENTS";
+	    ridgedEdge = false;
+	    metallurgy = "Cupro-Nickel";
+        frontMotto = "IN GOD WE TRUST";
+	    backMotto = "E PLURIBUS UNUM";
+	    frontLabel = "LIBERTY";
+	    backLabel = "UNITED STATES OF AMERICA";
     }
 
     public Nickel(int year) {
-        
+        familiarName = "Nickel";
+	    frontImage = "T_Jefferson";
+	    backImage = "Jefferson_Memorial";
+	    valueDescription = "FIVE CENTS";
+	    ridgedEdge = false;
+	    metallurgy = "Cupro-Nickel";
+        frontMotto = "IN GOD WE TRUST";
+	    backMotto = "E PLURIBUS UNUM";
+	    frontLabel = "LIBERTY";
+	    backLabel = "UNITED STATES OF AMERICA";
+        manufactureYear = year;
     }
 }
