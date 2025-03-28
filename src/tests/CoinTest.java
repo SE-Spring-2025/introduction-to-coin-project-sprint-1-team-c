e3024cce0e9f32328c1c99c74a48f7a50c7183c5
Carson Fouts <119092941+Fouts-C@users.noreply.github.com>
2025-03-28 12:51:00 -0400

    Update CoinTest.java

--- src/tests/CoinTest.java
+++ src/tests/CoinTest.java
@@ -14,11 +14,36 @@ public class CoinTest {
 
     @Test
     public void testConstructors() {
-	
+	// Make sure they don't blow up
+	// parameterless constructor
+	try {
+	    Penny p = new Penny();
+	}
+	catch (Exception e) {
+	    fail("Coin() constructor caused exception: "+e.getMessage());
+	}
+
+	// value arg constructor with year
+	try {
+	    Penny p = new Penny(2020);
+	}
+	catch (Exception e) {
+	    fail("Coin() constructor caused exception: "+e.getMessage());
+	}
     }
 
     @Test
     public void testGetters() {
+	
+	if (! testPenny()) fail("penny getters failed");
+	if (! testNickel()) fail("nickel getters failed");
+	if (! testDime()) fail("dime getters failed");
+	if (! testQuarter()) fail("quarter getters failed");
+	if (! testHalfDollar()) fail("half dollar getters failed");
+	if (! testDollar()) fail("dollar getters failed");
+	
+	// make it here then didn't fail!
+	assertTrue(true);
 
     }
 }
